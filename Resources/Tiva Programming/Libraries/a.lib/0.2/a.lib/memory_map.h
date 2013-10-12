e2dca7588be47dd8c0e25cc2cf74456b712ce218
Anthony <anthony.llanos@upr.edu>
2013-10-12 15:58:13 -0400

    Commiting Experiment 04 Implementation and BaseProject v1.0.1

---
+++ Resources/Tiva Programming/Libraries/a.lib/0.2/a.lib/memory_map.h
@@ -0,0 +1,23 @@
+/*
+ * memory_map.h
+ *
+ *  Created on: Oct 11, 2013
+ *      Author: Anthony.
+ */
+
+#ifndef MEMORY_MAP_H_
+#define MEMORY_MAP_H_
+
+/* Ranges from 0x00000000 to 0x0003FFFF.
+ * Reference: Page 539
+ */
+#define FLASH_ON_CHIP 0x000000000
+
+/* Ranges from 0x00000000 to 0x00000000.
+ * Reference: Page xxx
+ */
+#define FLASH_ON_CHIP 0x000000000
+
+
+
+#endif /* MEMORY_MAP_H_ */
---
+++ Resources/Tiva Programming/Libraries/a.lib/0.2/a.lib/timer.c
@@ -0,0 +1,10 @@
+/*
+ * timer.c
+ *
+ *  Created on: Oct 9, 2013
+ *      Author: Administrator
+ */
+
+#include "timer.h"
+
+
